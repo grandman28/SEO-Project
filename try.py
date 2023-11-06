476cb881eafb6b787c4fc7797a985e60373b9554
dsabina03 <dsabina1003@gmail.com>
2023-11-06 21:15:24 +0200

    site functional

--- try.py
+++ try.py
@@ -1,52 +1,44 @@
-from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 import requests
+from bs4 import BeautifulSoup
 
+app = Flask(__name__)
 
-app = Flask(_name_)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    result = None
-    if request.method == 'POST':
-        link = request.form.get('link')
-        result = link_checker(link)
-    return render_template('index.html', result=result)
-//
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
 
 def link_checker(link):
     try:
-        # GET request
+        if link[:8] != 'https://':
+            if link[:7] != 'http://':
+                s = 'http://'
+                link = s + link
+                print(link)
+            # GET request
         req = requests.get(link)
 
         # check status-code
         if req.status_code in [400, 404, 403, 408, 409, 501, 502, 503]:
-            print(f"{link} => Broken status-code: {req.status_code}")
+            return f"{link} => Broken status-code: {req.status_code}"
         else:
-            print(f"{link} => Good")
-
+            return f"{link} => Good"
+    
     # Exception
     except requests.exceptions.RequestException as e:
-        # print link with Errs
-        print(f"{link}: Something wrong \nErr: {e}")
-
-'''
-link = input("Enter link:")
-print("Link is: " + link)
-
-link_checker(link)
-
-url = link
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
-
-urls = []
-for link in soup.find_all('a'):
-    print(link.get('href'))
-'''
-if _name_ == '_main_':
-    app.run(debug=True)
+        return f"{link}: Something wrong \nErr: {e}"
+    
+@app.route("/result", methods=['POST', 'GET'])
+def result():
+    result = "No result"  
+    if request.method == 'POST':
+        link = request.form.get('link')
+        link_result = link_checker(link)
+        if link_result is not None:
+            result = link_result
+    return render_template("index.html", result=result)
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
